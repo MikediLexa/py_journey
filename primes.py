ca8387095c915a1dc4f3cf40978687f52c23b7b6
MikediLexa <mi.schneider.90@googlemail.com>
2025-03-04 23:05:21 +0100

    modules testet

--- primes.py
+++ primes.py
@@ -1,7 +1,8 @@
-for n in range (2,100):
-    for x in range (2,n):
-        if n %x == 0:
-            print(n, 'equals', x, '*', n//x)
-            break
-    else:
-        print(n, 'is a prime number')
+def primes(a,b):
+    for n in range (a,b):
+        for x in range (2,n):
+            if n %x == 0:
+                print(n, 'equals', x, '*', n//x)
+                break
+        else:
+            print(n, 'is a prime number')
--- tutorial_testing.py
+++ tutorial_testing.py
@@ -1,6 +1,11 @@
+from primes import primes as p
+
 def f(ham: str, eggs: str = 'eggs') -> str:
     print("Annotations:", f.__annotations__)
     print("Arguments:", ham, eggs)
     return ham + ' and ' + eggs
 
 f('spam')
+
+
+print(p(2,100))
